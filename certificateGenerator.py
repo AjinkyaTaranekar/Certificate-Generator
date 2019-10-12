9f3f1ea2838962ead8c2392e86eab39a6137e260
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2019-10-12 15:01:10 +0530

    multiple certificate

--- certificateGenerator.py
+++ certificateGenerator.py
@@ -4,7 +4,15 @@ from PIL import ImageDraw
 from google.cloud import vision
 from google.cloud.vision import types
 import io
-
+import xlrd 
+  
+# Give the location of the file 
+loc = ("sample.xlsx") 
+  
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+sheet.cell_value(0, 0) 
+  
 def assemble_word(word):
     assembled_word = ""
     for symbol in word.symbols:
@@ -21,40 +29,40 @@ def find_word_location(document, word_to_find):
                     if (assembled_word == word_to_find):
  		   	return word.bounding_box
 
+def findNameCol():
+	for i in range(sheet.ncols):
+		if(str(sheet.cell_value(0,i)).lower()=="name"):
+			return i
+
 
 vision_client = vision.ImageAnnotatorClient()
 
-with io.open('certificate.png', 'rb') as image_file:
-    content = image_file.read()
+image_file='certificate.png'
+with io.open(image_file, 'rb') as image_file2:
+    content = image_file2.read()
 
 content_image = types.Image(content=content)
 response = vision_client.document_text_detection(image=content_image)
 document = response.full_text_annotation
 
-location=(find_word_location(document, "that"))
-img = Image.open('certificate.png')
-draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 8, encoding="unic")
 
-draw.text((location.vertices[1].x +5,location.vertices[1].y-1), 'Ajinkya Taranekar', (0,0,0), font=font)
-img.save('out_file.png')
+for i in range(1,sheet.nrows): 
+	location=(find_word_location(document, "that"))
+	img = Image.open(image_file)
+	draw = ImageDraw.Draw(img)	
+	draw.text((location.vertices[1].x +5,location.vertices[1].y-1), str(sheet.cell_value(i,findNameCol())), (0,0,0), font=font)
+	
+	location=(find_word_location(document, "on"))
+	draw = ImageDraw.Draw(img)
+	draw.text((location.vertices[1].x +5,location.vertices[1].y -1), '12/10/2019', (0,0,0), font=font)
 
-location=(find_word_location(document, "on"))
+	location =find_word_location(document, "at")
+	draw = ImageDraw.Draw(img)
+	draw.text((location.vertices[1].x +5,location.vertices[1].y+20), 'Bhopal', (0,0,0), font=font)
 
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12, encoding="unic")
-draw.text((location.vertices[1].x +5,location.vertices[1].y -1), '12/10/2019', (0,0,0), font=font)
-img.save('out_file.png')
-
-location =find_word_location(document, "at")
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12, encoding="unic")
-draw.text((location.vertices[1].x +5,location.vertices[1].y+20), 'Bhopal', (0,0,0), font=font)
-img.save('out_file.png')
-
-location=find_word_location(document, "entitled")
-
-font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 12, encoding="unic")
-draw = ImageDraw.Draw(img)
-draw.text((location.vertices[1].x +5,location.vertices[1].y -1), "Version Beta", (0,0,0), font=font)
-img.save('out_file.png')
+	location=find_word_location(document, "entitled")
+	draw = ImageDraw.Draw(img)
+	draw.text((location.vertices[1].x +5,location.vertices[1].y -1), "Version Beta", (0,0,0), font=font)
+	output_image='out_file'+str(i)+'.png'	
+	img.save(output_image)
